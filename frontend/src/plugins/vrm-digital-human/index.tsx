ba66a79731c2f622ed892a7d9c255d7bbf326b6a
Catlina-2B <Catlina1996@163.com>
2026-04-27 02:19:38 +0800

    refactor: remove old dock system, replaced by grid layout

--- frontend/src/plugins/vrm-digital-human/index.tsx
+++ frontend/src/plugins/vrm-digital-human/index.tsx
@@ -1,10 +1,6 @@
-import DigitalHumanPanel from "./DigitalHumanPanel";
 import type { HermesFrontendPlugin } from "../types";
 
 export const vrmDigitalHumanPlugin: HermesFrontendPlugin = {
   id: "vrm-digital-human",
   name: "VRM Digital Human",
-  renderSidebarBottom: (context) => (
-    <DigitalHumanPanel expressionCallbackRef={context.chatDirectiveRef} />
-  ),
 };
